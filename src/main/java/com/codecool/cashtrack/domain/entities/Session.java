f018ca658720546a18fe11819131e02c512bcf78
Magvyy <MAHImprovement@gmail.com>
2026-02-26 20:15:44 +0100

    added a session entity, which is tied to a user, has a description, has a start and end time, and a list of exercises

--- src/main/java/com/codecool/cashtrack/domain/entities/Session.java
+++ src/main/java/com/codecool/cashtrack/domain/entities/Session.java
@@ -1,39 +1,57 @@
 package com.codecool.cashtrack.domain.entities;
 
-import com.codecool.cashtrack.application.DTOs.incoming.ExpenseRequestDTO;
+import com.codecool.cashtrack.domain.entities.enums.ExerciseEnum;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
 @Setter
 @Table(name = "expenses")
-public class Act {
+public class Session {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-
-    @Column(name = "cost", nullable = false)
-    private BigDecimal cost;
-
-    @Column(name = "description", nullable = true)
-    private String description;
-
-    @Column(name = "timestamp", nullable = false)
-    private LocalDateTime timestamp;
 
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
-    public Act(ExpenseRequestDTO expenseRequestDTO, User user) {
-        this.cost = expenseRequestDTO.getCost();
-        this.description = expenseRequestDTO.getDescription();
-        this.timestamp = expenseRequestDTO.getTimestamp();
+    @Column(name = "description", nullable = true)
+    private String description;
+
+    @Column(name = "start", nullable = false)
+    private ZonedDateTime start;
+
+    @Column(name = "end", nullable = false)
+    private ZonedDateTime end;
+
+    @OneToMany(mappedBy = "session")
+    private List<Exercise> exercises = new ArrayList<>();
+
+    public Session(User user, String description) {
         this.user = user;
+        this.description = description;
+        ZoneId timeZone = ZoneId.of("Europe/Oslo");
+        this.start = ZonedDateTime.now(timeZone);
+    }
+
+    public void endSession() {
+        ZoneId timeZone = ZoneId.of("Europe/Oslo");
+        this.end = ZonedDateTime.now(timeZone);
+    }
+
+    public void addExercise(Exercise exercise) {
+        this.exercises.add(exercise);
+    }
+
+    public void removeExercise(Exercise exercise) {
+        this.exercises.remove(exercise);
     }
 }
