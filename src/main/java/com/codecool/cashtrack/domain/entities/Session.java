6c4e0b84282e8ba9b78e3239a61b6f317e1cc875
Magvyy <MAHImprovement@gmail.com>
2026-02-26 20:32:34 +0100

    removed unused imports

--- src/main/java/com/codecool/cashtrack/domain/entities/Session.java
+++ src/main/java/com/codecool/cashtrack/domain/entities/Session.java
@@ -1,6 +1,5 @@
 package com.codecool.cashtrack.domain.entities;
 
-import com.codecool.cashtrack.domain.entities.enums.ExerciseEnum;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
