a9be42e7676df9d1e631225d3751c6f90b71bea9
Magvyy <MAHImprovement@gmail.com>
2026-02-26 20:01:05 +0100

    added weight to set

--- src/main/java/com/codecool/cashtrack/domain/entities/Set.java
+++ src/main/java/com/codecool/cashtrack/domain/entities/Set.java
@@ -5,6 +5,8 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.math.BigDecimal;
+
 
 @Entity
 @Getter
@@ -21,19 +23,24 @@ public class Set {
     @Column(name = "reps", nullable = false)
     private int reps;
 
+    @Column(name = "weight", nullable = false)
+    private BigDecimal weight;
+
     @ManyToOne
     @JoinColumn(name = "exercise_id", nullable = false)
     private Exercise exercise;
 
-    public Set(SetEnum setType, int reps, Exercise exercise) {
+    public Set(SetEnum setType, int reps, BigDecimal weight, Exercise exercise) {
         this.setType = setType;
         this.reps = reps;
+        this.weight = weight;
         this.exercise = exercise;
     }
 
-    public Set(int reps, Exercise exercise) {
+    public Set(int reps, BigDecimal weight, Exercise exercise) {
         this.setType = SetEnum.NORMAL;
         this.reps = reps;
+        this.weight = weight;
         this.exercise = exercise;
     }
 }
