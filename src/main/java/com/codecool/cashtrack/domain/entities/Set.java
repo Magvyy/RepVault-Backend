e5a539ed808c5118284e8c177854e5c9e757af15
Magvyy <MAHImprovement@gmail.com>
2026-02-26 19:55:14 +0100

    added set with set type, an exercise it is tied to and number of reps

---
+++ src/main/java/com/codecool/cashtrack/domain/entities/Set.java
@@ -0,0 +1,39 @@
+package com.codecool.cashtrack.domain.entities;
+
+import com.codecool.cashtrack.domain.entities.enums.SetEnum;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Entity
+@Getter
+@Setter
+@Table(name = "expenses")
+public class Set {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private SetEnum setType;
+
+    @Column(name = "reps", nullable = false)
+    private int reps;
+
+    @ManyToOne
+    @JoinColumn(name = "exercise_id", nullable = false)
+    private Exercise exercise;
+
+    public Set(SetEnum setType, int reps, Exercise exercise) {
+        this.setType = setType;
+        this.reps = reps;
+        this.exercise = exercise;
+    }
+
+    public Set(int reps, Exercise exercise) {
+        this.setType = SetEnum.NORMAL;
+        this.reps = reps;
+        this.exercise = exercise;
+    }
+}
